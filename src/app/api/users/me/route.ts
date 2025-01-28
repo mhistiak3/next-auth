dd3ba415d16db409cd7a2f939b3ca2351d2171b5
Istiak <mhistiak2@gmail.com>
2025-01-28 19:33:25 +0600

    complete backend

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import connectDB from "@/config/mongodb.config";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import { getDataFromToken } from "@/utils/tokenHandler";
+// conneting to the database
+connectDB();
+
+export const POST = async (request: NextRequest) => {
+  try {
+    // get userId
+    const userId = getDataFromToken(request);
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    // get user
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 404 });
+    }
+
+    NextResponse.json({
+      success: true,
+      user,
+    });
+  } catch (error: any) {
+    NextResponse.json({ message: error.message }, { status: 500 });
+  }
+};
---
+++ src/utils/tokenHandler.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import jwt from "jsonwebtoken"
+import { NextRequest } from "next/server"
+
+export const getDataFromToken = (request:NextRequest) => {
+try {
+    const token = request.cookies.get("token")?.value || ""
+    if (!token) {
+        throw new Error("Token not found")
+    }
+    const decodedToken:any = jwt.verify(token, process.env.JWT_SECRET!)
+
+    return decodedToken.userId
+
+} catch (error:any) {
+    throw new Error(error.message)
+}
+}
