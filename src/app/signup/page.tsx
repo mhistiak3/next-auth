1e354372c1f1b837b6b08fb175ac294e577ee1a4
Istiak <mhistiak2@gmail.com>
2025-01-29 23:06:36 +0600

    sign up functionality

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+  const router = useRouter()
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoding] = useState(false);
+  const isDisabled = !user.username || !user.email || !user.password;
+
+  const handleSubmit =async (e: any) => {
+    e.preventDefault();
+    try {
+      setLoding(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log(response.data);
+      if (response.data) {
+        toast.success("User created successfully");
+      }
+      router.push("/login");
+    } catch (error:any) {
+      console.log(error);
+      
+    } finally {
+      setLoding(false);
+    }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <Link
@@ -11,7 +44,7 @@ export default function SignupPage() {
       </Link>
       <div className="w-full max-w-md bg-gray-800 p-8 rounded-xl shadow-lg">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-300 mb-2" htmlFor="username">
               Username
@@ -21,6 +54,8 @@ export default function SignupPage() {
               type="text"
               className="w-full px-4 py-2 bg-gray-700 rounded-lg border border-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
               placeholder="Enter your username"
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
             />
           </div>
           <div className="mb-4">
@@ -32,6 +67,8 @@ export default function SignupPage() {
               type="email"
               className="w-full px-4 py-2 bg-gray-700 rounded-lg border border-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
               placeholder="Enter your email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
             />
           </div>
           <div className="mb-4">
@@ -43,11 +80,14 @@ export default function SignupPage() {
               type="password"
               className="w-full px-4 py-2 bg-gray-700 rounded-lg border border-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
               placeholder="Enter your password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
             />
           </div>
           <button
             type="submit"
             className="w-full px-4 py-2 bg-purple-600 rounded-lg hover:bg-purple-700 text-white font-bold"
+            disabled={isDisabled || loading}
           >
             Sign Up
           </button>
