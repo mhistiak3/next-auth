bb8306ed7d0087cc6d1d3f0619ec002f7782e805
Istiak <mhistiak2@gmail.com>
2025-02-01 21:14:03 +0600

    Add login functionality and user profile page with lucide-react icons

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      console.log(user);
+      const response = await axios.post("/api/users/login", user);
+
+      if (response.data) {
+        toast.success("Login Successfull");
+        setTimeout(() => {
+          router.push("/me");
+        }, 2000);
+      }
+    } catch (error: any) {
+      console.log(error);
+      toast.error("Invalid email or password");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <Link href="/" className="absolute top-4 left-4 text-purple-500 hover:underline">
-      Back To Home
+      <Link
+        href="/"
+        className="absolute top-4 left-4 text-purple-500 hover:underline"
+      >
+        Back To Home
       </Link>
       <div className="w-full max-w-md bg-gray-800 p-8 rounded-xl shadow-lg">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-300 mb-2" htmlFor="email">
-              Email
+            <label className="block text-gray-300 mb-2" htmlFor="username">
+              Username
             </label>
             <input
-              id="email"
-              type="email"
+              id="username"
+              type="text"
               className="w-full px-4 py-2 bg-gray-700 rounded-lg border border-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
-              placeholder="Enter your email"
+              placeholder="Enter your username"
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
             />
           </div>
           <div className="mb-4">
@@ -29,11 +64,14 @@ export default function LoginPage() {
               type="password"
               className="w-full px-4 py-2 bg-gray-700 rounded-lg border border-gray-600 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
               placeholder="Enter your password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
             />
           </div>
           <button
             type="submit"
             className="w-full px-4 py-2 bg-purple-600 rounded-lg hover:bg-purple-700 text-white font-bold"
+            disabled={loading}
           >
             Login
           </button>
